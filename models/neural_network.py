9b7120895946e63d5445e6d8d5c0bf801d2529e9
Francesco Rondini <frondini23@gmail.com>
2025-11-20 12:48:00 +0100

    Inserimento nn

---
+++ models/neural_network.py
@@ -0,0 +1,16 @@
+import torch.nn as nn
+
+class NeuralNetwork(nn.Module):
+    def __init__(self, input_size, num_classes):
+        super().__init__()
+        self.net = nn.Sequential(
+            nn.Linear(input_size, 512),
+            nn.ReLU(),
+            nn.Linear(512, 256),
+            nn.ReLU(),
+            nn.Linear(256, num_classes)
+        )
+
+
+    def forward(self, x):
+        return self.net(x)
