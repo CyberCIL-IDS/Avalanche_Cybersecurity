216f93ae1d8587d5257c617113e4a3ae58414185
Luigi Lauriola <luigilauriola22@gmail.com>
2025-11-20 17:27:35 +0100

    Aggiunta commenti

--- utils/train.py
+++ utils/train.py
@@ -16,7 +16,7 @@ def train():
     # Istanzia il modello
     model = NeuralNetwork()
 
-    # Imposta ottimizzatore
+    # Imposta Algoritmo di ottimizzazione
     optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
 
     # Plugin per logging ed evaluation
@@ -28,24 +28,24 @@ def train():
 
     # Strategy Replay — continual learning
     strategy = Replay(
-        model=model,
-        optimizer=optimizer,
-        criterion=torch.nn.CrossEntropyLoss(),
-        train_mb_size=64,
-        train_epochs=5,
-        eval_mb_size=64,
-        evaluator=eval_plugin,
-        device="cuda" if torch.cuda.is_available() else "cpu",
-        mem_size=2000   # buffer di replay
+        model = model,
+        optimizer = optimizer,
+        criterion = torch.nn.CrossEntropyLoss(),    #funzione di loss: funzione che misura quanto il modello “sbaglia” durante il training.
+        train_mb_size = 64,     # Dimensione batch training - numero di esempi per ogni passo di addestramento
+        train_epochs = 5,       # Epoche per esperienza - quante volte l’esperienza viene ripassata
+        eval_mb_size = 64,      # Dimensione batch per valutazione - per ridurre uso memoria durante test
+        evaluator = eval_plugin,
+        device = "cuda" if torch.cuda.is_available() else "cpu",
+        mem_size = 2000   # buffer di replay
     )
 
-    # Training sulle esperienze incrementali
-    for experience in benchmark.train_stream:
+    # Training sulle esperienze incrementali - Questo ciclo simula perfettamente il continual learning.
+    for experience in benchmark.train_stream:       #Per ogni esperienza experience
         print(f"\n Training su: {experience}")
-        strategy.train(experience)
+        strategy.train(experience)  #addestra la rete sull'esperienza attuale - mescola i dati correnti con quelli del replay buffer - aggiorna il buffer con nuovi esempi
 
         print("\nEvaluation:")
-        strategy.eval(benchmark.test_stream)
+        strategy.eval(benchmark.test_stream)    #valuta il modello su tutte le esperienze viste e non viste - mostra metriche grazie all’InteractiveLogger
 
     print("\nTraining completato con Replay Strategy + NeuralNetwork.")
 
